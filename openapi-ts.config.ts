d5d38d0944dce0110faac58f8f74563e6f66abbe
Sebastian Jäger <sebastjaeger@gmail.com>
2026-03-08 18:33:48 +0100

    Fix env samples and openapi config

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  input: "../openapi.json",
+  input: "./openapi.json",
   output: "./src/client",
   plugins: ["@hey-api/client-fetch"],
 });
